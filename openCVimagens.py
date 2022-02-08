2548cae4c44a73627c5df5f595ba65f01e8292e1
Diogo M. França <diogo.meneses.franca@gmail.com>
2022-02-08 00:34:52 -0300

    Aprendendo e testando escalas de cor

--- openCVimagens.py
+++ openCVimagens.py
@@ -1,13 +1,33 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 #CARREGANDO UMA IMAGEM E REDIMENCIONANDO
-imagem = cv2.imread("pessoas faces.jpg")
-redimencionada = cv2.resize(imagem, (600,400))
-cv2.imshow("Faces", redimencionada)
-cv2.waitKey(0)
+imagem = cv2.imread("pessoas faces.jpg",0)
+#coresReversas = imagem[:,:, ::-1]
+#redimencionada = cv2.resize(imagem, (1366,768))
+#cv2.imshow("Faces", imagem)
+#cv2.waitKey(0)
+plt.imshow(imagem)
+plt.waitforbuttonpress()
+#MOSTRA A MATRIZ DA IMAGEM
+#print(imagem)
 
-#MUDANDO A ESCALA DE COR
-redi_cinza = cv2.cvtColor(redimencionada, cv2.COLOR_BGR2GRAY)
-cv2.imshow("cinza", redi_cinza)
-cv2.waitKey(0)
 
-#UNINDO IMAGENS
+#MOSTRA O TAMANHO DA IMAGEM
+print("Tamanho da imagem:", imagem.shape)
+print("Tipo de dado da imagem:", imagem.dtype)
+#plt.imshow(coresReversas)
+#plt.waitforbuttonpress()
+
+#SEPARANDO A IMAGEM EM ESCALAS DE CORES
+"""b,g,r = cv2.split(redimencionada)
+plt.figure(figsize=[20,5])
+plt.subplot(141);plt.imshow(r,cmap = 'gray'); plt.title("Vermelha")
+plt.subplot(142);plt.imshow(g,cmap = 'gray'); plt.title("Verde")
+plt.subplot(143);plt.imshow(b,cmap = 'gray'); plt.title("Azul")
+misturada = cv2.merge((b,g,r))
+plt.subplot(144);plt.imshow(misturada[:,:,::-1]),plt.title("Fundidas")
+plt.waitforbuttonpress()"""
+
+#SALVANDO A IMAGEM
+#cv2.imwrite("imagemCinza.jpg", redimencionada)
