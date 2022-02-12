bd4f15e9c0118883ac5d2a82a61d6dc8e7f75f5d
Diogo M. França <diogo.meneses.franca@gmail.com>
2022-02-12 11:59:01 -0300

    mostrando pro yago

--- openCVimagens.py
+++ openCVimagens.py
@@ -4,30 +4,25 @@ import numpy as np
 #CARREGANDO UMA IMAGEM E REDIMENCIONANDO
 imagem = cv2.imread("pessoas faces.jpg",0)
 #coresReversas = imagem[:,:, ::-1]
-#redimencionada = cv2.resize(imagem, (1366,768))
-#cv2.imshow("Faces", imagem)
-#cv2.waitKey(0)
-plt.imshow(imagem)
-plt.waitforbuttonpress()
-#MOSTRA A MATRIZ DA IMAGEM
-#print(imagem)
-
-
-#MOSTRA O TAMANHO DA IMAGEM
-print("Tamanho da imagem:", imagem.shape)
-print("Tipo de dado da imagem:", imagem.dtype)
+redimencionada = cv2.resize(imagem, (400,300))
+#APRESENTANDO TODAS AS ESCALAS DE COR DA OPENCV
+#escalasDeCor = cv2.cvtColor(imagem,cv2.COLOR_BGR2YCR_CB)
+#COMO MOSTRAR A IMAGEM NA TELA COM AS DUAS BIBLIOTECAS OPENCV E MATPLOTLIB
+cv2.imshow("Faces", redimencionada)
+cv2.waitKey(0)
 #plt.imshow(coresReversas)
 #plt.waitforbuttonpress()
 
-#SEPARANDO A IMAGEM EM ESCALAS DE CORES
-"""b,g,r = cv2.split(redimencionada)
-plt.figure(figsize=[20,5])
-plt.subplot(141);plt.imshow(r,cmap = 'gray'); plt.title("Vermelha")
-plt.subplot(142);plt.imshow(g,cmap = 'gray'); plt.title("Verde")
-plt.subplot(143);plt.imshow(b,cmap = 'gray'); plt.title("Azul")
-misturada = cv2.merge((b,g,r))
-plt.subplot(144);plt.imshow(misturada[:,:,::-1]),plt.title("Fundidas")
-plt.waitforbuttonpress()"""
+#MOSTRA A MATRIZ DA IMAGEM
+#print(redimencionada)
+
+
+#MOSTRA O TAMANHO DA IMAGEM
+#print("Tamanho da imagem:", redimencionada.shape)
+#print("Tipo de dado da imagem:", redimencionada.dtype)
+#plt.imshow(coresReversas)
+#plt.waitforbuttonpress()
+
 
 #SALVANDO A IMAGEM
-#cv2.imwrite("imagemCinza.jpg", redimencionada)
+cv2.imwrite("imagemCinza.jpg", redimencionada)
