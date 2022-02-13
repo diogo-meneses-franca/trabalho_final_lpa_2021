699837a209a82df0fa01ae5919de8409d948cccf
Diogo M. França <diogo.meneses.franca@gmail.com>
2022-02-12 21:37:06 -0300

    Finalização do trabalho

--- openCVimagens.py
+++ openCVimagens.py
@@ -1,28 +1,47 @@
 import cv2
+from cv2 import addWeighted
 import matplotlib.pyplot as plt
-import numpy as np
-#CARREGANDO UMA IMAGEM E REDIMENCIONANDO
-imagem = cv2.imread("pessoas faces.jpg",0)
-#coresReversas = imagem[:,:, ::-1]
+"""#CARREGANDO UMA IMAGEM E REDIMENCIONANDO
+imagem = cv2.imread("pessoas faces.jpg",cv2.IMREAD_COLOR)
+coresReversas = imagem[:,:, ::-1]
 redimencionada = cv2.resize(imagem, (400,300))
 #APRESENTANDO TODAS AS ESCALAS DE COR DA OPENCV
-#escalasDeCor = cv2.cvtColor(imagem,cv2.COLOR_BGR2YCR_CB)
+escalasDeCor = cv2.cvtColor(imagem,cv2.COLOR_BGR2YCR_CB)
 #COMO MOSTRAR A IMAGEM NA TELA COM AS DUAS BIBLIOTECAS OPENCV E MATPLOTLIB
-cv2.imshow("Faces", redimencionada)
-cv2.waitKey(0)
-#plt.imshow(coresReversas)
-#plt.waitforbuttonpress()
+#cv2.imshow("Faces", redimencionada)
+#cv2.waitKey(0)
+plt.imshow(coresReversas)
+plt.waitforbuttonpress()"""
 
 #MOSTRA A MATRIZ DA IMAGEM
-#print(redimencionada)
+"""pequena = cv2.imread('imagem18x18.png',0)
+plt.imshow(pequena, cmap = 'gray')
+print(pequena)"""
+
 
 
 #MOSTRA O TAMANHO DA IMAGEM
 #print("Tamanho da imagem:", redimencionada.shape)
 #print("Tipo de dado da imagem:", redimencionada.dtype)
-#plt.imshow(coresReversas)
-#plt.waitforbuttonpress()
+
+#CORTANDO UMA IMAGEM
+barquinho_bgr = cv2.imread('barquinho.jpg', cv2.IMREAD_COLOR)
+barquinho_rgb = barquinho_bgr[:,:, ::-1]
+cortada = barquinho_rgb[200:400, 300:600]
+plt.imshow(cortada)
+plt.waitforbuttonpress(0)
+
+
+#UNINDO IMAGENS DIFERENTES
+"""frente = cv2.imread('frente.jpg')
+fundo = cv2.imread('fundo.jpg')
+frente_red = cv2.resize(frente,(1366,768), interpolation= cv2.INTER_AREA)
+fundo_red = cv2.resize(fundo,(1366,768), interpolation= cv2.INTER_AREA)
+fusao = cv2.addWeighted(fundo_red, 0.5, frente_red, 0.8, 0.0)
+cv2.imshow("Juntas", fusao)
+cv2.waitKey(0)"""
+
 
 
 #SALVANDO A IMAGEM
-cv2.imwrite("imagemCinza.jpg", redimencionada)
+#cv2.imwrite("imagemCinza.jpg", redimencionada)
--- openCvcamera.py
+++ openCvcamera.py
@@ -1,11 +1,20 @@
 import cv2
 
-imagem = cv2.VideoCapture(0)
-while (True):
-    retorno, frame = imagem.read()
+camera = cv2.VideoCapture(0)
+algTreinado = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    captura, frame = camera.read()
+    cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detecta = algTreinado.detectMultiScale(cinza)
+    for ( x, y, l, a ) in detecta:
+        cv2.rectangle(frame, (x, y), (x + l, y + a), (0,255,0),2)
+        
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) == ord("q"):
         break
 
-imagem.release()
+camera.release()
 cv2.destroyAllWindows()
+
+
